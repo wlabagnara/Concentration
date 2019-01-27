d85fbecc78a688f3a4cc9c9df28ec33299087b22
wlabagnara <wlabagnara@yahoo.com>
2019-01-26 23:41:11 -0500

    card color changed from orange to blue

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         } else {
             button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
