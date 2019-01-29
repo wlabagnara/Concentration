a8f8d7bb5ddfeaa664958370030cc8966ff2f05d
wlabagnara <wlabagnara@yahoo.com>
2019-01-28 23:03:54 -0500

    Updating concentration app to end of lecture 1.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,13 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func touchCard(_ sender: UIButton) {
-        flipCard(withEmoji: "👻", on: sender)
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["👻","🎃","👻","🎃"]
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var flipCount = 0
+    {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButtons.firstIndex(of: sender)
+        {
+            print(" Index of touched card is \(cardNumber)" )
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+        else
+        {
+            print(" Cannot find card index of touched card!" )
+        }
+    }
+
     
     func flipCard (withEmoji  emoji: String, on button: UIButton) {
         
+        flipCount += 1
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
